f4c2b900495c8a65a1b814c163caa32326160e16
Leonardo Saganski <lsaganski@gmail.com>
2017-01-02 02:40:18 -0200

    quase na segunda previa

--- config/server.js
+++ config/server.js
@@ -3,16 +3,25 @@ var json = require('json-middleware').middleware();
 var urlencode = require('urlencode');
 var bodyparser = require('body-parser');
 var app = express();
-app.use(bodyparser.json());
+//var multer = require('multer');
+var path = require('path');
+var formidable = require('formidable');
+var fs = require('fs');
+
 app.use(bodyparser.urlencoded({
 	extended: true
 }));
+app.use(bodyparser.json());
+app.use(express.static(path.join(__dirname, 'public')));   //__dirname + '/public'
+
 
 var database = require('./dbConnection')();
 
 //----------------------------------------------------------
 
 var query = require('../app/models/query');
+
+var sucess = true;
 
 // Login
 app.post('/api/login/', function(req, res) {
@@ -22,6 +31,57 @@ app.post('/api/login/', function(req, res) {
  		password = req.body.password;
 
 	conn.query('SELECT * FROM AIDAVEC_USER WHERE USR_EMAIL = \'' + username + '\' AND USR_SENHA = \'' + password + '\'', function(err,result){
+		return res.json(result);
+	});
+});
+
+app.post('/api/upload', function(req, res){
+
+  // create an incoming form object
+  var form = new formidable.IncomingForm();
+
+  // specify that we want to allow the user to upload multiple files in a single request
+  form.multiples = true;
+
+  // store all uploads in the /uploads directory
+  form.uploadDir = path.join(__dirname, '/uploads');
+
+  // every time a file has been uploaded successfully,
+  // rename it to it's orignal name
+  form.on('file', function(field, file) {
+    fs.rename(file.path, path.join(form.uploadDir, file.name));
+  });
+
+  // log any errors that occur
+  form.on('error', function(err) {
+    console.log('An error has occured: \n' + err);
+  });
+
+  // once all the files have been uploaded, send a response to the client
+  form.on('end', function() {
+    res.end('success');
+  });
+
+  // parse the incoming request containing the form data
+  form.parse(req);
+
+});
+
+app.get('/api/images/:file', function (req, res){
+    file = req.params.file;
+    var dirname = path.join(__dirname, '/uploads');
+    var img = fs.readFileSync(path.join(__dirname, '/uploads/' + file));
+    res.writeHead(200, {'Content-Type': 'image/jpg' });
+    res.end(img, 'binary');
+});
+
+// Check if email is already regsitered
+app.post('/api/checkemail/', function(req, res) {
+	var conn = database();
+
+	var email = req.body.email;
+
+	conn.query('SELECT * FROM AIDAVEC_USER WHERE USR_EMAIL = \'' + email + '\'', function(err,result){
 		return res.json(result);
 	});
 });
@@ -38,16 +98,59 @@ app.get('/api/:tablename', function(req, res) {
 });
 
 // CRUD Insert
-app.post('/api/:tablename', function(req, res) {
+app.post('/api/user', function(req, res) {
 	var conn = database();
 
- 	var tablename = req.params.tablename, 
- 		data = req.body;
+ 	var data = req.body;
 
- 		console.log(data);
+	conn.query('INSERT INTO AIDAVEC_USER SET ? ', [data], function(err,result){
 
-	conn.query('INSERT INTO ' + tablename + ' SET ? ', [data], function(err,result){
+		SendMail(data.USR_EMAIL, result.insertId);
+
 		return res.json(result);
+	});
+});
+
+// CRUD Insert
+app.post('/api/waypoints', function(req, res) {
+	var conn = database();
+
+ 	var data = req.body; 
+
+ 	var result = 'T';
+
+ 	success = true;
+
+ 	for (var index in data.waypoints) {
+ 		var waypoint = data.waypoints[index];
+
+ 		SaveWaypoint(waypoint, conn);
+
+ 		if (success == false)
+ 			break;
+	};
+
+	return res.sendStatus(success ? 200 : 501);
+});
+
+function SaveWaypoint(data, conn) {
+	conn.query('INSERT INTO AIDAVEC_WAYPOINT SET ? ', [data], function(err,result){
+
+		if (err)
+			success = false;
+
+		return true;
+	});
+}
+
+// Ativar cadastro
+app.get('/api/active/user/:id', function(req, res) {
+	var conn = database();
+
+	var id = req.params.id;
+
+	conn.query('UPDATE AIDAVEC_USER SET USR_STATUS = 1 WHERE USR_ID = ' + id, function(err,result){
+		return res.end("<html><body>Cadastro ativado com sucesso.</body></html>");
 	});
 });
 
@@ -75,6 +178,32 @@ app.delete('/api/:tablename/:id', function(req, res) {
 
 });
 //------------------------------------------------------
+
+function SendMail(address, id) {
+	var nodemailer = require('nodemailer');
+
+   	var transporter = nodemailer.createTransport({
+	    host: 'smtp.mobila.com.br',
+	    port: 587,
+	    secure: false, // use SSL
+	    auth: {
+	        user: 'contato@mobila.com.br',
+	        pass: '1978@Mobila'
+	    }
+	});
+
+	var email = {
+  		from: 'contato@mobila.com.br', // Quem enviou este e-mail
+  		to: address, // Quem receberá
+  		subject: 'Confirme seu cadastro',  // Um assunto bacana :-) 
+  		html: 'Bem vindo !<br><br>Você fez um novo cadastro no Aidavec.<br><br>Para começar a usar o aplicativo é necessário ativar seu cadastro, clicando no link abaixo :<br><br><a href="http://www.mobila.kinghost.net/aidavecapi/api/active/user/' + id + '">Ativar cadastro</a>' // O conteúdo do e-mail
+	};
+
+	transporter.sendMail(email, function(err, info){
+  		if(err)
+    		throw err; // Oops, algo de errado aconteceu.
+	});
+}
 
 module.exports = app;
 
